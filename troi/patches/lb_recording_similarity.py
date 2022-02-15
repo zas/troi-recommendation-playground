4eea80fc150707d7018ff1f5455c03007666feb7
Robert Kaye <rob@musicbrainz.org>
2022-02-15 16:00:15 +0100

    Add missing file

--- troi/patches/lb_recording_similarity.py
+++ troi/patches/lb_recording_similarity.py
@@ -19,12 +19,16 @@ class LBRecordingSimilarityPatch(troi.patch.Patch):
     @staticmethod
     @cli.command(no_args_is_help=True)
     @click.argument('recording_mbid')
+    @click.argument('window_size', default=1)
+    @click.argument('filter_artists', default=1)
     def parse_args(**kwargs):
         """
         Find similar recordings from ListenBrainz.
 
         \b
         RECORDING_MBID: A musicbrainz recording MBID to find similar recordings to
+        WINDOW_SIZE: Which window size to choose. Must be 1, 5 or 10
+        FILTER_ARTISTS: Filter out same artists. Must be 0 or 1
         """
         return kwargs
 
@@ -38,12 +42,16 @@ class LBRecordingSimilarityPatch(troi.patch.Patch):
 
     def create(self, inputs, patch_args):
         recording_mbid = inputs['recording_mbid']
+        window_size = inputs['window_size']
+        filter_artists = inputs['filter_artists']
 
         source = DataSetFetcherElement(server_url="https://bono.metabrainz.org/recording-similarity/json",
-                                       json_post_data=[{ 'recording_mbid': inputs['recording_mbid'] }])
+                                       json_post_data=[{ 'recording_mbid': recording_mbid,
+                                                         'window_size': str(window_size),
+                                                         'filter_same_artist': str(filter_artists)}])
 
         pl_maker = troi.playlist.PlaylistMakerElement("Similar recording test playlist",
-                                                      "Similar recordings to %s" % recording_mbid,
+                                                      "Similar recordings to %s. Window size %d, filter artists %d." % (recording_mbid, window_size, filter_artists),
                                                       patch_slug=self.slug())
         pl_maker.set_sources(source)
 
